684db20146eb0867eeaca5ab1b203b30a675cc3b
Jordan Simpson <jordan.simpson.dev@gmail.com>
2023-03-03 15:55:47 -0600

    Add files via upload

---
+++ src/main/java/com/github/j0rdanit0/chatgptdiscordbot/ChatgptDiscordBotApplication.java
@@ -0,0 +1,13 @@
+package com.github.j0rdanit0.chatgptdiscordbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptDiscordBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptDiscordBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/j0rdanit0/chatgptdiscordbot/ChatgptDiscordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.j0rdanit0.chatgptdiscordbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatgptDiscordBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
